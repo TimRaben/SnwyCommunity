331780d443c72ebe321cb0846363aef44f7fd426
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-16 22:17:18 +0100

    init1
    init1

---
+++ commands/ticketClose.js
@@ -0,0 +1,22 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const categoryID = "910270086605062235";
+
+    if (message.channel.parentID == categoryID) {
+        message.channel.delete();
+    } else {
+
+
+        message.channel.send(":x: **|** Doe dit in een officiële ticket!");
+    
+    }
+
+}
+
+module.exports.help = {
+    name: "close",
+    description: "Geeft al de verschillende commands",
+    category: "Informatie",
+}
