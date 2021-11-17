aa8ce452a8b672ad2d089bf9eecbc4011ea075f4
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-17 16:42:15 +0100

    init1
    init1

--- index.js
+++ index.js
@@ -50,7 +50,35 @@ client.on("ready", async () => {
         client.user.setActivity(status, { type: "WATCHING"}) // Can Be WATCHING, STREAMING, LISTENING
     }, 4500) // Second You Want to Change Status, This Cahnges Every 2 Seconds
 
-});               
+});        
+
+var channelID = '910542711046680637'
+
+    var regelsChannel = '845680452882792508'
+
+    var rrChannel = '846021919194218526'
+
+    var ticketChannel = '870005075748216963'
+
+    var promotieChannel = '894513156536295425'
+
+    const welkomEmbed = new discord.MessageEmbed()
+        .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
+        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+        .setTitle(`Welkom ${member.author}`)
+        .setDescription(`Hey Hallo! Leuk dat je onze Server bent gejoined, je bent van harte welkom! Lees hieronder enige welkoms informatie waar je welke dingen kunt vinden!\n\n**Navigatie**\n• Regels **|** ${member.guild.channels.cache.get(regelsChannel).toString()}\n• Support/Hulp **|** ${member.guild.channels.cache.get(ticketChannel).toString()}\n• Marketing Info **|** ${member.guild.channels.cache.get(promotieChannel).toString()}\n• Reactie Rollen **|** ${member.guild.channels.cache.get(rrChannel).toString()}`)
+        .setColor("PURPLE")
+        .setFooter("Snwy Community - Welkom")
+        .setImage("https://media.discordapp.net/attachments/909775422047256576/910548369754169364/Snwy_Community_banner.gif")
+        .setTimestamp();
+
+        client.on('guildMemberAdd', (member) => {
+            console.log(member)
+
+    const channel = member.guild.channels.cache.get(channelID)
+    channel.send(welkomEmbed)
+
+    });
 
 client.on("message", async message => {
 
@@ -92,34 +120,5 @@ client.on("message", async message => {
     if (commands) commands.run(client, message, arguments);
 
 });
-
-var channelID = '910542711046680637'
-
-    var regelsChannel = '845680452882792508'
-
-    var rrChannel = '846021919194218526'
-
-    var ticketChannel = '870005075748216963'
-
-    var promotieChannel = '894513156536295425'
-
-
-    client.on('guildMemberAdd', (member) => {
-        console.log(member)
-
-    const welkomEmbed = new discord.MessageEmbed()
-        .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-        .setTitle(`Welkom ${member.author}`)
-        .setDescription(`Hey Hallo! Leuk dat je onze Server bent gejoined, je bent van harte welkom! Lees hieronder enige welkoms informatie waar je welke dingen kunt vinden!\n\n**Navigatie**\n• Regels **|** ${member.guild.channels.cache.get(regelsChannel).toString()}\n• Support/Hulp **|** ${member.guild.channels.cache.get(ticketChannel).toString()}\n• Marketing Info **|** ${member.guild.channels.cache.get(promotieChannel).toString()}\n• Reactie Rollen **|** ${member.guild.channels.cache.get(rrChannel).toString()}`)
-        .setColor("PURPLE")
-        .setFooter("Snwy Community - Welkom")
-        .setImage("https://media.discordapp.net/attachments/909775422047256576/910548369754169364/Snwy_Community_banner.gif")
-        .setTimestamp();
-
-    const channel = member.guild.channels.cache.get(channelID)
-    channel.send(welkomEmbed)
-
-    })
 
 client.login(process.env.token);
