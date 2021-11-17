448728cd2328c99cfa6c7e5ea003742c0fa617de
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-17 16:43:25 +0100

    init1
    init1

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ var channelID = '910542711046680637'
 
     const welkomEmbed = new discord.MessageEmbed()
         .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+        .setThumbnail(member.author.displayAvatarURL({ dynamic: true }))
         .setTitle(`Welkom ${member.author}`)
         .setDescription(`Hey Hallo! Leuk dat je onze Server bent gejoined, je bent van harte welkom! Lees hieronder enige welkoms informatie waar je welke dingen kunt vinden!\n\n**Navigatie**\n• Regels **|** ${member.guild.channels.cache.get(regelsChannel).toString()}\n• Support/Hulp **|** ${member.guild.channels.cache.get(ticketChannel).toString()}\n• Marketing Info **|** ${member.guild.channels.cache.get(promotieChannel).toString()}\n• Reactie Rollen **|** ${member.guild.channels.cache.get(rrChannel).toString()}`)
         .setColor("PURPLE")
