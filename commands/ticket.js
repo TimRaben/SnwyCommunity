030dd40e2fbcb44230f8f7b46f2abadf98089f31
TimRaben <timraben07@gmail.com>
2021-11-27 16:33:34 +0100

    init1
    init1

--- commands/ticket.js
+++ commands/ticket.js
@@ -8,24 +8,9 @@ module.exports.run = async (client, message, args) => {
     var user = message.author;
     var userDiscriminator = message.author.discriminator;
 
-    var meldingBestaat = false;
+    message.channel.send(':white_check_mark: **|** Je hebt succesvol een ticket gemaakt!')
 
-    message.guild.channels.cache.forEach(channel => {
-
-        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
-            meldingBestaat = true;
-
-            message.reply("Je hebt al een ticket in de category **Overig**!");
-
-            setTimeout(() => {
-
-                message.channel.bulkDelete(2);
-
-            }, 3000);
-
-        }
-
-    message.guild.channels.create("ticket" + "-" + "overig", { type: 'text' }).then(
+    message.guild.channels.create(userName.toLowerCase() + "-overig", { type: 'text' }).then(
         (createdChannel) => {
             createdChannel.setParent(categoryID).then(
                 (settedParent) => {
@@ -46,28 +31,28 @@ module.exports.run = async (client, message, args) => {
                     });
 
                     var embedParent = new discord.MessageEmbed()
-                        .setTitle(`Ticket Systeem - Snwy Community`)
-                        .setDescription(`Hallo ${message.author.username}, Hieronder worden automatisch vragen gesteld, geef hier zo duidelijk en uitgebreid mogelijk antwoord op waardoor wij je z.s.m. kunnen helpen!`)
-                        .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
-                        .setFooter('Snwy Community - Ticket Systeem')
-
-                    var staff = "870021783649148968";
-                    var person = message.author;
-
-                    var vraag1o = new discord.MessageEmbed()
-                        .setTitle("Ticket Systeem - Overig")
-                        .setColor("PURPLE")
-                        .setDescription("Waarmee kunnen we u van dienst zijn?")
-                    
-                    var vraag2o = new discord.MessageEmbed()
-                        .setTitle("Ticket Systeem - Overig")
-                        .setColor("PURPLE")
-                        .setDescription("Wat is de precieze reden dat je deze ticket hebt aangemaakt?")
-                    
-                    var vraag3o = new discord.MessageEmbed()
-                        .setTitle("Ticket Systeem - Overig")
-                        .setColor("PURPLE")
-                        .setDescription("Wil je nog iets toevoegen? Zo niet typ dan **Nee**")
+                                    .setTitle(`Ticket Systeem - Snwy Community`)
+                                    .setDescription(`Hallo ${message.author.username}, Hieronder worden automatisch vragen gesteld, geef hier zo duidelijk en uitgebreid mogelijk antwoord op waardoor wij je z.s.m. kunnen helpen!`)
+                                    .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
+                                    .setFooter('Snwy Community - Ticket Systeem')
+            
+                                var staff = "870021783649148968";
+                                var person = message.author;
+            
+                                var vraag1o = new discord.MessageEmbed()
+                                    .setTitle("Ticket Systeem - Vraag")
+                                    .setColor("PURPLE")
+                                    .setDescription("Wat is je vraag?")
+                                
+                                var vraag2o = new discord.MessageEmbed()
+                                    .setTitle("Ticket Systeem - Vraag")
+                                    .setColor("PURPLE")
+                                    .setDescription("Aan wie is je vraag gericht? (bijv moderators, admins, developers, managers)")
+                                
+                                var vraag3o = new discord.MessageEmbed()
+                                    .setTitle("Ticket Systeem - Vraag")
+                                    .setColor("PURPLE")
+                                    .setDescription("Wil je nog iets toevoegen? Zo niet typ dan **Nee**")
 
                     settedParent.send(message.author);
                     settedParent.send(`${user}`);
@@ -75,28 +60,26 @@ module.exports.run = async (client, message, args) => {
                     settedParent.send(vraag1o);
 
                     settedParent.awaitMessages(s => s.author.id == message.author.id, { max: 1 }).then(antwoord => {
-                        var antwoord1o = antwoord.first();;
+                        var antwoord1 = antwoord.first();;
                         settedParent.send(vraag2o);
                     
                         settedParent.awaitMessages(s => s.author.id == message.author.id, { max: 1 }).then(antwoord => {
-                            var antwoord2o = antwoord.first();;
+                            var antwoord2 = antwoord.first();;
                             settedParent.send(vraag3o);
                         
                         settedParent.awaitMessages(s => s.author.id == message.author.id, { max: 1 }).then(antwoord => {
-                                var antwoord3o = antwoord.first();;
+                                var antwoord3 = antwoord.first();;
 
                                 var overigeticket = new discord.MessageEmbed()
-                                    .setTitle("Ticket Systeem - Overig")
-                                    .setColor("PURPLE")
-                                    .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
-                                    .setThumbnail("https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
-                                    .setDescription(`Hallo! Bedankt voor het aanmaken van een ticket, je hebt zojuist een aantal vragen ingevuld, Staff zou je z.s.m. helpen!\n\n**Waarmee Staff kan helpen:**\n${antwoord1o}\n\n**Precieze Reden van deze Ticket:**\n${antwoord2o}\n\n**Eventuele Toevoegingen:**\n${antwoord3o}\n\n*Een van onze Staffleden zal je z.s.m. helpen!*`)
-
-                                settedParent.bulkDelete(6).then(
-                                    settedParent.send("<@&870021783649148968>", overigeticket)
+                                        .setTitle("Ticket Systeem - Overig")
+                                        .setColor("PURPLE")
+                                        .setAuthor("Snwy Community", "https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
+                                        .setThumbnail("https://tikstar-user-images.oss-cn-hongkong.aliyuncs.com/92b3_6982470257928504321.jpg")
+                                        .setDescription(`Hallo! Bedankt voor het aanmaken van een ticket, je hebt zojuist een aantal vragen ingevuld, Staff zou je z.s.m. helpen!\n\n**Waarmee Staff kan helpen:**\n${antwoord1o}\n\n**Precieze Reden van deze Ticket:**\n${antwoord2o}\n\n**Eventuele Toevoegingen:**\n${antwoord3o}\n\n*Een van onze Staffleden zal je z.s.m. helpen!*`)
+            
+                                        settedParent.bulkDelete(6).then(
+                                            settedParent.send("<@&870021783649148968>", overigeticket)
                                 )
-                            })
-
                                 })
                             })
                         })
