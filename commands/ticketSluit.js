ba167cdb11512848a0982e2cb4c3b409fe17b4c5
TimRaben <timraben07@gmail.com>
2021-11-27 16:41:22 +0100

    init1
    init1

--- commands/ticketSluit.js
+++ commands/ticketSluit.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    const categoryID = "910270086605062235";
+    const categoryID = "914169722197671957";
 
     if (message.channel.parentID == categoryID) {
         message.channel.delete();
